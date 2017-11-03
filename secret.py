f006c0bad4e03fb5940afa4c1e9a98a96ad8d6a6
Tanupriya <tanraj8@gmail.com>
2017-11-03 22:53:15 +0530

    File names modified
    The files in the folder are arranged as specified in the function(e.g., deleting digits from image's name.)

---
+++ secret.py
@@ -0,0 +1,11 @@
+import os
+def rename_files():
+    #step1
+    file_list=os.listdir(r"Your home directory")
+    saved_path=os.getcwd()
+    os.chdir(r"Your home driectory")
+    #step2
+    for file_name in file_list:
+        os.rename(file_name,file_name.translate(None,"0123456789"))
+        print(file_name.translate(None,"0123456789"))
+rename_files()
